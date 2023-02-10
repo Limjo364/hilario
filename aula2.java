cbba951263583fb0fee55c40f9f03952d4d86eaf
Limjo364 <124941066+Limjo364@users.noreply.github.com>
2023-02-10 01:04:09 +0000

    teste

---
+++ aula2.java
@@ -0,0 +1,8 @@
+public class aula2 {
+
+    public static void main(String[] args) {
+        String nome;
+        nome = "Teste";
+        System.out.println(nome);
+    }
+}
---
+++ parte2.java
@@ -0,0 +1,9 @@
+public class parte2 {
+    public static void main(String[] args){
+        int contador = 0;
+        while(contador < 100) {
+        System.out.println("bom dia");
+        contador++;
+        }
+    }
+}
