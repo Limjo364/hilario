27fea4ef30bfaf8163a76f630a91c200382c23cc
Limjo364 <124941066+Limjo364@users.noreply.github.com>
2023-02-10 01:23:16 +0000

    teste

---
+++ media.java
@@ -0,0 +1,9 @@
+public class media {
+    public static void main(String[] args){
+        int n1, n2, m;
+        n1 = 8;
+        n2 = 6;
+        m = (n1+n2)/2;
+        System.out.println(m);
+    }
+}
