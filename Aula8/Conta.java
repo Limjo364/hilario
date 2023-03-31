e1ab61ad07d73f2324ea27a4615440062107f038
Limjo364 <124941066+Limjo364@users.noreply.github.com>
2023-03-31 01:21:59 +0000

    aula8

--- Aula8/Conta.java
+++ Aula8/Conta.java
@@ -1,14 +1,16 @@
 package Aula8;
 
 public class Conta {
-    String numero;
+    private String nro;
     double saldo;
-    public Conta (String nro, double saldoInicial){
-            numero = nro;
-            saldo = saldoInicial;}
-    void credito (double valor){
-        saldo = saldo+valor;}
-    void debito (double valor){
-        saldo = saldo-valor;}
+    public Conta (String nro, double saldo){
+            this.nro = nro;
+            this.saldo = saldo;}
+    public void depositar (double valor){
+            this.saldo+=valor;}
+    public void sacar (double valor){
+            this.saldo-=valor;}
+    public double getSaldo(){
+            return this.saldo;}
     
     }
--- Aula8/CriaConta.java
+++ Aula8/CriaConta.java
@@ -2,11 +2,11 @@ package Aula8;
 
 public class CriaConta {
     public static void main(String[] args){
-        Conta c1 = new Conta();
-        c1.numero = "21.345-7";
-        c1.saldo = 0;
-        c1.credito (500.87);
-        c1.debito (45.00);
-        System.out.println(c1.saldo);
+        Conta c1 = new Conta("1234", 100);
+        c1.saldo=200;
+        c1.depositar(100);
+        c1.sacar(50);
+        System.out.println("Saldo atual: R$"+c1.getSaldo());
+        
     }
 }
